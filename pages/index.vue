56d2e6895d88d1e1fce8ef41b14636d5bc2107aa
Jorge <jorgermalves@protonmail.com>
2024-09-10 11:24:44 +0200

    feat(index): add Head component with google fonts link components

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
+  <Head>
+    <link
+      rel="preconnect"
+      href="https://fonts.googleapis.com"
+    >
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossorigin="true"
+    >
+  </Head>
   <Quiz />
 </template>
