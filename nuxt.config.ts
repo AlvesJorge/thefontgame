a05798f67ece8584d91a2af419420aa909ffde3c
Jorge <jorgermalves@protonmail.com>
2024-09-16 13:12:13 +0200

    switch to client side rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
