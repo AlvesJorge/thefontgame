719e6cdda08dac593b0f6e859cb1f705af7685bf
Jorge <jorgermalves@protonmail.com>
2024-09-23 23:44:00 +0100

    fix crossoring value

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
-        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
+        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous" },
         { rel: "author", href: "https://alvesjorge.com" }
       ],
       script: [
