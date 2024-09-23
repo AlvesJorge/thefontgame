00eeec42a046ebd7bc684cf68f8b31da35f69946
Jorge <jorgermalves@protonmail.com>
2024-09-23 23:55:20 +0100

    use refs in stats.vue

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,14 +1,15 @@
 <script setup>
-
 import allFontNames from "~/assets/fonts.json";
-const fontNames = allFontNames["fonts"];
+const fontNames = ref(allFontNames["fonts"]);
 const historyStore = useFontHistoryStore();
-const statsStore = useStatsStore();
-const seenFonts = new Set(historyStore.history);
+const statsStore = ref(useStatsStore());
+const seenFonts = ref(new Set(historyStore.history));
+
 function clearData() {
   window.localStorage.clear();
   window.location.reload();
 }
+
 </script>
 <template>
   <div>
@@ -55,7 +56,7 @@ function clearData() {
           <p>All this data is stored in your browser.</p>
           <p>Although the space it uses is negligible, you may delete it by using the button below</p>
           <br>
-          <Button @click="()=> clearData()">
+          <Button @click="clearData">
             Reset Stats and Options
           </Button>
         </CardContent>
@@ -64,32 +65,32 @@ function clearData() {
   </div>
 </template>
 <style scoped>
-#main{
-  display:grid;
-  place-content:center;
-  height:90%;
+#main {
+  display: grid;
+  place-content: center;
+  height: 90%;
   width: 80%;
 
-  margin:0 auto;
-  padding:2rem;
-  gap:2rem;
+  margin: 0 auto;
+  padding: 2rem;
+  gap: 2rem;
 }
-#statsTable{
-  margin-top:2rem;
-  font-size:5rem !important;
-  border-radius:1rem;
-  border:2px solid hsl(var(--border));
-  width:100%;
+#statsTable {
+  margin-top: 2rem;
+  font-size: 5rem !important;
+  border-radius: 1rem;
+  border: 2px solid hsl(var(--border));
+  width: 100%;
 }
 #statsTable * {
-  font-size:1.2rem;
+  font-size: 1.2rem;
 }
 
 @media only screen and (max-width: 800px) {
   #main {
     padding: 1rem;
     gap: 1rem;
-    width:95%
+    width: 95%;
   }
 }
 </style>
